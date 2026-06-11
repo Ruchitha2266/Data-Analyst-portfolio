823838830e220c16bb658aaf7ab41e1e6cfd002c
Ruchitha2266 <ruchithayadav208@gmail.com>
2026-06-11 15:05:17 +0530

    Add files via upload

---
+++ sales_analysis.py
@@ -0,0 +1,7 @@
+sales = [1000, 1500, 2000, 1200, 1800]
+
+total_sales = sum(sales)
+average_sales = total_sales / len(sales)
+
+print("Total Sales:", total_sales)
+print("Average Sales:", average_sales)
